8c098b25d2aaf313a79e1c424e68c2335bd38316
dracarys18 <karthihegde010@gmail.com>
2020-07-08 00:35:07 +0530

    tweet_bot: Modify the bot for sending the tweets over telegram
    Signed-off-by: dracarys18 <karthihegde010@gmail.com>

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ import logging
 from telegram.ext import Updater
 import os
 from dotenv import load_dotenv
+from tweet_scrape import TweetBot
+import time
 
-load_dotenv("keys.env")
-API_TOKEN = str(os.getenv("TELEGRAM_BOT"))
-updater = Updater(token='API_TOKEN',use_context=True)
-
+if __name__ == '__main__':
+    user_list = ["utdreport","manutd"]
+    load_dotenv("keys.env")
+    API_TOKEN = str(os.getenv("TELEGRAM_BOT"))
+    updater = Updater(bot=TweetBot(user_list,API_TOKEN),use_context=True)
+    dispatcher = updater.dispatcher
+    yes = TweetBot(user_list,API_TOKEN)
+    while True:
+        yes.fetch_tweets()
+        time.sleep(5)
+    
--- tweet_scrape.py
+++ tweet_scrape.py
@@ -1,22 +1,48 @@
 import os
 import tweepy as tp
 from dotenv import load_dotenv
+from telegram import Bot
+from botsql.db_utils import Users
 
-load_dotenv("keys.env")
-consumer_key = str(os.getenv("CONSUMER_KEY"))
-consumer_secret = str(os.getenv("CONSUMER_SECRET"))
-auth = tp.OAuthHandler(consumer_key,consumer_secret)
-api = tp.API(auth)
-users = ["utdreport","manutd"]
-results =[]
+class TweetBot(Bot):
 
-try:
-    for i in users:
-        twit=api.user_timeline(screen_name=i,count=20,include_retweets=True,tweet_mode = 'extended')
-        results.append(twit)
-except tp.error.TweepError as err:
-    print("K")
+    def __init__(self,user_list,token):
+        super().__init__(token=token)
+        self.userslist = list(user_list)
 
-for k in results:
-    for i in k:
-        print(i.full_text+"\n")
+    def fetch_tweets(self):
+        load_dotenv("keys.env")
+        consumer_key = str(os.getenv("CONSUMER_KEY"))
+        consumer_secret = str(os.getenv("CONSUMER_SECRET"))
+        auth = tp.OAuthHandler(consumer_key,consumer_secret)
+        api = tp.API(auth)
+        userid = 0
+        results = []
+        last_tweet = self.Users.get_lastweet(ac_id=1)
+        chatid = -1001418848817
+        try:
+            for i in self.userslist:
+                if last_tweet!=0:
+                    twit=api.user_timeline(screen_name=i,count=20,max_id=last_tweet-1,include_retweets=True,tweet_mode = 'extended')
+                    results.append(twit)
+                else:
+                    twit = api.user_timeline(screen_name=i,count=20,include_retweets=True,tweet_mode = 'extended')
+                    results.append(twit)
+        except tp.error.TweepError as err:
+            print(str(err))
+        for k in results:
+            for i in k:
+                data = {
+                    'tweet_id'  : i.id ,
+                    'name'  : i.user.name,             
+                    'text'  : i.full_text
+                }
+                self.Users.add_to_db(ac_id=userid,ac_name=i.screen_name,last_tweet=data['tweet_id'])
+                userid=userid+1
+                self.sendMessage(chat_id=chatid,text=str(data['text'])+"Via"+" ["+str(data['name'])+" ]",timeout=200)
+    
+
+
+    
+
+
