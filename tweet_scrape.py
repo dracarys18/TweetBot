e369bba0ea75ca84bcfde473d2f1b434f62c83ef
dracarys18 <karthihegde010@gmail.com>
2020-07-10 10:48:56 +0530

    tweet_bot: Stream tweets asynchornously
    * Acc to the docs stream wont terminate unless the connection is closed
    
    Signed-off-by: dracarys18 <karthihegde010@gmail.com>

--- tweet_scrape.py
+++ tweet_scrape.py
@@ -60,7 +60,7 @@ class TweetBot():
         listener = TwitterStream()
         account_list = self.get_tweet_acid(self.userslist)
         stream_tweet = tp.Stream(api,listener,tweet_mode='extended')
-        stream_tweet.filter(follow=account_list)
+        stream_tweet.filter(follow=account_list,is_async=True)
 
     def get_tweet_acid(self,user_list):
         api = self.authorize()
