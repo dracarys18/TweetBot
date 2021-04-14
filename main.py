a5788854372b7aa8928fb2530d063d19c513ddc7
dracarys18 <karthihegde010@gmail.com>
2021-04-14 16:24:26 +0530

    Catch protocol error
    Signed-off-by: dracarys18 <karthihegde010@gmail.com>

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import telegram
 import logging
 from telegram.ext import Updater,CommandHandler
+from urllib3.exceptions import ProtocolError
 import os
 import tweet_scrape as ts
 from dotenv import load_dotenv
@@ -17,5 +18,9 @@ if __name__ == '__main__':
     updater = Updater(token=API_TOKEN,use_context=True)
     dispatcher = updater.dispatcher
     while True:
-        startbot()
-        time.sleep(5)
+        try:
+            startbot()
+            time.sleep(5)
+        except ProtocolError:
+            continue
+        
