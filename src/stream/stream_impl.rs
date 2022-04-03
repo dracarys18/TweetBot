455ba87e9b1703718918fab0bfc72c28d23dac90
dracarys18 <karthihegde010@gmail.com>
2022-04-03 23:56:23 +0530

    Only send the tweet if the reply is from the tweeter

--- src/stream/stream_impl.rs
+++ src/stream/stream_impl.rs
@@ -15,16 +15,20 @@ pub async fn stream_tweets(config: &Config, to_follow: Vec<u64>) -> TweetResult<
         .try_for_each(|t| async move {
             println!("here");
             if let StreamMessage::Tweet(tweet) = t {
-                let tweeter_name = &tweet.user.as_ref().expect("Invalid User").name;
+                let tweeter = tweet.user.as_ref().expect("Invalid User");
+                let name = &tweeter.name;
+                let _user_id = tweeter.id;
                 let tweet_url = get_tweet_url(&tweet).await.unwrap_or_default();
                 let text = escape(&tweet.text);
                 let urls = get_url_entity(&tweet).await.unwrap_or_default();
                 let message = format!(
                     "{}\n{}\nVia |<a href='{}'>{}</a>|",
-                    text, urls, tweet_url, tweeter_name
+                    text, urls, tweet_url, name
                 );
-                //Don't send retweeted tweets
-                if matches!(tweet.retweeted, Some(false)) {
+                //Don't send retweeted tweets and only send if the reply is from the current user (in case of threads)
+                if matches!(tweet.retweeted, Some(false))
+                    || matches!(tweet.in_reply_to_user_id, Some(_user_id))
+                {
                     bot.send_message(config.chat_id, message)
                         .parse_mode(ParseMode::Html)
                         .await
